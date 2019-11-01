970e09898c1dd61ec8944b3bf3835b206c75d33f
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-11-01 14:02:12 -0600

    gallery

--- cit230/lesson7/scripts/lazyload.js
+++ cit230/lesson7/scripts/lazyload.js
@@ -1,29 +1,33 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOption = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
 
-  if('IntersectionObserver' in window) {
+if ('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
     });
     imagesToLoad.forEach((img) => {
-      observer.observe(img);
+        observer.observe(img);
     });
-  } else {
+} else {
     imagesToLoad.forEach((img) => {
-      loadImages(img);
+        loadImages(img);
     });
-  }
+}
+
+

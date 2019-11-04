c3310511ddcc1b30c156324d75d8c27943ab0d5e
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-11-04 10:29:34 -0700

    add 8

--- cit230/lesson8 copy/scripts/pancake.js
+++
@@ -1,6 +0,0 @@
-// pancake code:: show/hide pancake
-var weekDayNumber = currentDate.getDay();
-
-if (weekDayNumber === 5){
-    document.getElementById("pancake").removeAttribute("class","hidden");
-}

b2286b14070bf48d5c7dbd0dae1726fda7ae96b7
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-11-27 19:57:45 -0700

    errors

--- cit230/lesson11/WeatherSite/scripts/weatherSummary.js
+++ cit230/lesson11/WeatherSite/scripts/weatherSummary.js
@@ -34,6 +34,10 @@ var month = months[monthNumber];
 
 var year = currentDate.getFullYear();
 var date = currentDate.getDate();
+ /* ------------------------------------------
+                    Footer Date Calculation
+               --------------------------------------
+ ------------------------------------------------------------------------------------------------------------------------------ */
 
 currentDateString = weekDay + ', ' + date + ' ' + month + ' ' + year;
 document.getElementById('currentDate').innerHTML = currentDateString;
@@ -43,6 +47,11 @@ function toggleMenu() {
   
 }
 
+ /* ------------------------------------------------------------------------------------------------------------------------------
+            ------------------------------------------
+                    5 Day Forecast Script
+               --------------------------------------
+ ------------------------------------------------------------------------------------------------------------------------------ */
 
 currentDay = (currentDate.getDay())+1;
 var daysInputted = 0;
@@ -61,3 +70,24 @@ for( i=1; currentDay <= 6; i++){
         currentDay++;
         remaniderDays--;
     } 
+
+
+let currentTemp = 41;
+let windSpeed = 8;
+let high = 50;
+let humididy = 5+"%";
+let windChill = 0;
+
+if (currentTemp < 50 && windSpeed > 3){
+let windChill = Math.round(35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * currentTemp * Math.pow(windSpeed, 0.16)));
+}
+
+document.getElementById("Forecast1").innerHTML = "Current Temperature: " + currentTemp + '&deg;';
+document.getElementById("Forecast2").innerHTML = "High: " + high;
+document.getElementById("Forecast3").innerHTML = "Wind Chill: " + windChill + '&deg;';
+document.getElementById("Forecast4").innerHTML = "Humidity: " + humididy;
+if (windChill ===0){
+    document.getElementById("Forecast5").innerHTML = "Wind Chill:  N/A";
+}
+document.getElementById("Forecast5").innerHTML = "Wind Speed: " + windSpeed + "mph";
+

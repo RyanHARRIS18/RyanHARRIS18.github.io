96fb4c27b0e0d7081268724b3591f3a4d0233270
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-11-27 18:04:44 -0700

    weathersite1

---
+++ cit230/lesson11-The Weather Site/Wether Site/week11/scripts/addClassAndID.js
@@ -0,0 +1,4 @@
+function giveClassAndId(){
+        var x = document.getElementsByClassName("section")[0].id;
+        document.getElementById("section").innerHTML = x;
+      }
---
+++ cit230/lesson11-The Weather Site/Wether Site/week11/scripts/retrievedata.js
@@ -0,0 +1,53 @@
+const request = "https://ryanharris18.github.io/cit230/lesson9/Week09/scripts/towndata.json";
+fetch(request)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+
+
+    for (let i = 0; i < 3; i++ ) {
+      let section = document.createElement('section');
+      section.setAttribute('id', towns[i].name + "section");
+      section.setAttribute('class', "homepage-sections");
+     
+
+     let h3 = document.createElement('h3');
+      h3.textContent = towns[i].name;
+      section.appendChild(h3);
+
+      let motto  = document.createElement('p');
+      motto.textContent = towns[i].motto;
+      motto.setAttribute('class', "motto");
+      section.appendChild(motto);
+
+      let yearFounded  = document.createElement('p');
+      yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+      yearFounded.setAttribute('class', "yearFounded");
+      section.appendChild(yearFounded);
+
+      let currentPopulation  = document.createElement('p');
+      currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+      currentPopulation.setAttribute('class', "currentPopulation");
+      section.appendChild(currentPopulation);
+
+      let averageRainfall  = document.createElement('p');
+      averageRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+      averageRainfall.setAttribute('class', "averageRainfall");
+      section.appendChild(averageRainfall);
+
+      let image = document.createElement('img');
+      image.setAttribute('src', towns[i].photo);
+      image.setAttribute('alt', towns[i].name);
+      image.setAttribute('id', towns[i].name + "Img");
+      image.setAttribute('class', "homepage-images");
+
+      section.appendChild(image);
+      
+      document.querySelector('div.cards').appendChild(section);
+
+
+    }
+  });

462302c9989750376cc479fd7c5ba59ec27c1bb2
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-12-09 13:35:17 -0700

    json switch

--- cit230/Templ Inn & Suites/scripts/retrievejsondata.js
+++ cit230/Templ Inn & Suites/scripts/retrievejsondata.js
@@ -10,8 +10,11 @@ fetch(templesRequest)
 
     for (let i = 0; i < 4; i++ ) {
       let section = document.createElement('section');
-      section.setAttribute('id', temples[i].name + "section");
+      section.setAttribute('id', "section" + [i]);
       section.setAttribute('class', "homepage-sections");
+
+      // let href = documnet.createElement('a');
+      // href.setAttribute('href'= './' + toLowerCase(temples[i].name));
 
       let image = document.createElement('img');
       image.setAttribute('src', temples[i].photo);
@@ -23,20 +26,33 @@ fetch(templesRequest)
 
      let h3 = document.createElement('h3');
       h3.textContent = temples[i].name;
+      h3.setAttribute('class', "temples-headers");
       section.appendChild(h3);
+
+      let h4 = document.createElement('h4');
+      h4.textContent = 'Address';
+      h4.setAttribute('class', "temples-headers");
+      section.appendChild(h4);
 
       let address  = document.createElement('p');
       address.textContent = temples[i].address;
-      address.setAttribute('class', "address");
+      address.setAttribute('class', "temples-p");
       section.appendChild(address);
 
+      let h42 = document.createElement('h4');
+      h42.textContent = 'Milestones';
+      h42.setAttribute('class', "temples-headers");
+      section.appendChild(h42);
       
       let Milestones  = document.createElement('p');
-      Milestones.textContent = 'Milestones: ' + temples[i].Milestones;
-      Milestones.setAttribute('class', "Milestones");
+      Milestones.textContent = temples[i].Milestones;
+      Milestones.setAttribute('class', "temples-p");
       section.appendChild(Milestones);
 
-    
+      // let link  = document.createElement('a');
+      // link.textContent = 'Dedicatory Prayer';
+      // link.href = temples[i].Milestone-Prayer;
+      // section.appendChild(link);
 
       document.querySelector('div.temple-cards').appendChild(section);
     }

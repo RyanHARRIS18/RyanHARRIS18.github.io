6af66150393d6fcbdb6a1068eb20fbe1bf94212b
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-12-11 14:41:03 -0700

    add to json and selector

--- cit230/TempleInn&Suites/scripts/retrieveCityjsondata.js
+++ cit230/TempleInn&Suites/scripts/retrieveCityjsondata.js
@@ -3,15 +3,22 @@ var i;
 // rome italy
 if (document.getElementById("cityID").value === '6542288') {
     makeCard(3);
+    makePriceCard(3);
 }
 else if (document.getElementById("cityID").value === '5577147') {
     makeCard(2);
+    makePriceCard(2);
+
 }
 else if (document.getElementById("cityID").value === '5391811') {
     makeCard(1);
+    makePriceCard(1);
+
 }
 else if(document.getElementById("cityID").value === '5780993') {
     makeCard(0);
+    makePriceCard(0);
+
 }
 
 function makeCard(cardNumber){
@@ -64,20 +71,50 @@ fetch(templesRequest)
       Milestones.setAttribute('class', "temples-p");
       section.appendChild(Milestones);
 
-      let MilestonePrayer  = document.createElement('p');
-      MilestonePrayer.textContent = temples[cardNumber].MilestonePrayer;
-      MilestonePrayer.setAttribute('class', "temples-p");
-      MilestonePrayer.setAttribute('class', "prayer-link");
-      section.appendChild(MilestonePrayer);
-
-
-      // let iframe = document.createElement('iframe');
-      // iframe.setAttribute('src', temples[cardNumber].Map);
-      // section.appendChild(iframe);
-
-      
 
       document.querySelector('div.temple-cards').appendChild(section);
     
   });
+}
+
+  
+
+        function makePriceCard(cardNumber){
+          const templesRequest = "https://raw.githubusercontent.com/RyanHARRIS18/RyanHARRIS18.github.io/master/cit230/TempleInn%26Suites/json/temples.json";
+        fetch(templesRequest)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (jsonObject) {
+          // console.table(jsonObject);  // temporary checking for valid response and data parsing
+          const temples = jsonObject['temples'];
+          for (let i = 0; i < 8; i++ ) {
+            let section = document.createElement('section');
+            section.setAttribute('id', "priceSection"[i] + [cardNumber]);
+            section.setAttribute('class', "homepage-sections");
+
+            // let href = documnet.createElement('a');
+            // href.setAttribute('href'= './' + toLowerCase(temples[cardNumber].name));
+            
+          let h3 = document.createElement('h3');
+          h3.textContent = temples[cardNumber].roomType[i];
+          h3.setAttribute('class', "temples-headers");
+          section.appendChild(h3);
+
+            let image = document.createElement('img');
+            image.setAttribute('src', temples[cardNumber].roomTypePhoto[i]);
+            image.setAttribute('alt', temples[cardNumber].roomType[i] + "photo");
+            image.setAttribute('id', "roomType"[i] + "Img");
+            image.setAttribute('class', "homepage-images");
+            section.appendChild(image);
+          
+
+            let h4 = document.createElement('h4');
+            h4.textContent = 'Price' + temples[cardNumber].price[i];
+            h4.setAttribute('class', "temples-headers");
+            section.appendChild(h4);
+
+            document.querySelector('div.hotel-Card-Holder').appendChild(section);
+   }
+  })
 }

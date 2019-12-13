7916b1f1519de8566549c1af9e848fbe2466d569
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2019-12-13 02:27:23 -0700

    update wave

--- cit230/TempleInn&Suites/scripts/retrieveCityjsondata.js
+++ cit230/TempleInn&Suites/scripts/retrieveCityjsondata.js
@@ -4,21 +4,22 @@ var i;
 if (document.getElementById("cityID").value === '6542288') {
     makeCard(3);
     makePriceCard(3);
+    makeGallery(3);
 }
 else if (document.getElementById("cityID").value === '5577147') {
     makeCard(2);
     makePriceCard(2);
-
+    makeGallery(2);
 }
 else if (document.getElementById("cityID").value === '5391811') {
     makeCard(1);
     makePriceCard(1);
-
+    makeGallery(1);
 }
 else if(document.getElementById("cityID").value === '5780993') {
     makeCard(0);
     makePriceCard(0);
-
+    makeGallery(0);
 }
 
 function makeCard(cardNumber){
@@ -35,9 +36,6 @@ fetch(templesRequest)
       let section = document.createElement('section');
       section.setAttribute('id', "section" + [cardNumber]);
       section.setAttribute('class', "homepage-sections");
-
-      // let href = documnet.createElement('a');
-      // href.setAttribute('href'= './' + toLowerCase(temples[cardNumber].name));
 
       let image = document.createElement('img');
       image.setAttribute('src', temples[cardNumber].photo);
@@ -71,7 +69,7 @@ fetch(templesRequest)
       Milestones.textContent = temples[cardNumber].Milestones;
       Milestones.setAttribute('class', "temples-p");
       section.appendChild(Milestones);
-      
+
       document.querySelector('div.temple-cards').appendChild(section);
     
   });
@@ -117,3 +115,32 @@ fetch(priceRequest)
     }
 })
 }
+
+function makeGallery(cardNumber){
+  const galleryRequest = "https://raw.githubusercontent.com/RyanHARRIS18/RyanHARRIS18.github.io/master/cit230/TempleInn%26Suites/json/temples.json";
+  fetch(galleryRequest)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    var temples = jsonObject['temples'];
+    var j = 0;
+     for (i in temples)  {
+      let section = document.createElement('section');
+      section.setAttribute('id', "gallerySection"+[j]);
+      section.setAttribute('class', "gallery-pages-sections");
+  
+      let image = document.createElement('img');
+      image.setAttribute('src', temples[cardNumber].gallery[j]);
+      image.setAttribute('alt', 'Gallery Image of' + temples[cardNumber].name + [j]);
+      image.setAttribute('id', 'gallery' +[i]);
+      image.setAttribute('class', "gallery-images");
+      section.appendChild(image);
+       
+      document.querySelector('section.gallery-Card-Holder').appendChild(section);
+      j++;
+  
+      }
+  })
+  }

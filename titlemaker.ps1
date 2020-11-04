b3fb622d553baf5f5f1a31790819c6e8ea40e3c6
RyanHARRIS18 <ryanpaulharris18@gmail.com>
2020-11-04 10:24:00 -0700

    Update titlemaker.ps1

--- titlemaker.ps1
+++ titlemaker.ps1
@@ -1,46 +1,71 @@
 Function formatTitle(){
-    param( [string]$misformatedString)
-    Write-Host $misformatedString
-    <# Make the string an array & make an array of the common words that are not to be 
-    captilized in titles Articles, Coordinate Conjuncctions ( aka FANBOYS), & Prepositions #>
-    $misformatedStringArray = $misformatedString.ToCharArray() | %{[char]$_}
-    write-host "Print out the array"
-    
-    $misformatedString.ToCharArray() | %{[char]$_}
+    param( 
+        [Parameter(Mandatory=$true)][string]$Title
+        #[Parameter(Mandatory=$false)][string]$AllCaps
+    )
+    $TitleArray = @();
+    $wordCount = ([regex]::Matches($Title, " " )).count;
+    for($i = 0; $i -lt $Title.Length; $i++){
+        $TitleArray += $Title.substring($i,1);
+    }
     $nonCapWords = 'a', 'an', 'the', 'for', 'and', 'nor', 'but', 'or', 'yet', 'so', 'at', 'around', 'by', 'after', 'along', 'for', 'from', 'of', 'on', 'to', 'with', 'without';
     $evaluatedWordsArray = @();
-    $indicateNewWord = "-------"
-    Write-Host $indicateNewWord
-$i = 0;    
-foreach($char in $misformatedStringArray){
-    write-host "char equals:" $char
-    write-host "Counter equals:" $i
-    write-host $misformatedStringArray[$char]; 
-             write-host "start word index:" $i
-             $length = misformatedStringArray.Length;
-             $currentIndex = $i;
-                for($j = $currentIndex; $j -lt $length; $j++)
-                {
-                    $i++
-                    if($misformatedStringArray[$j] -eq " "){
-                        $indicateNewWord
-                        continue;
-                    }
-                $newWord += $misformatedStringArray[$i];
-                write-host "new Word:" $newWord
-         }
-        $evaluatedWordsArray += $newWord;
-        #$i++
-        $indicateNewWord = "-------"
+             $TitleArray += " ";
+             $length = $TitleArray.Length;
+             $newWord = '';
+    for($j = 0; $j -lt $length+1; $j++)
+    {
+        if($TitleArray[$j] -eq " "){
+            $evaluatedWordsArray += $newWord;
+            $newWord = "";
+            continue;
+        }
+        else{
+        $newWord += $TitleArray[$j];
+        }
     }
-    $i = 0;
-  foreach($word in $evaluatedWordsArray){
-      if($i -eq 0 -or $nonCapWords.Contains($word)){
-        $word = $word.substring(0,1).toupper()+$word.substring(1).tolower();
-        $i++ 
-      }
-      foreach($word in $evaluatedWordsArray){ write-host "This is the evaluate word " $word}
-    }
-    $evaluatedWordsArray = @();
+ $i = 0;
+            foreach($word in $evaluatedWordsArray){
+                if($i -eq 0 -or !($nonCapWords.Contains($word.toLower()))){
+                    $word = $word.substring(0,1).toupper()+$word.substring(1).toLower();
+                    $correctCaptilization += $word;
+                    $i++ 
+                    $correctCaptilization += " ";
+                }
+                else{
+                $correctCaptilization += $word.toLower();
+                $correctCaptilization += " ";
+                }
+                }
+                $correctCaptilization
 
+
+# Switch($num){
+#     $Title{
+#         $i = 0;
+#             foreach($word in $evaluatedWordsArray){
+#                 if($i -eq 0 -or !($nonCapWords.Contains($word.toLower()))){
+#                     $word = $word.substring(0,1).toupper()+$word.substring(1).toLower();
+#                     $correctCaptilization += $word;
+#                     $i++ 
+#                     $correctCaptilization += " ";
+#                 }
+#                 else{
+#                 $correctCaptilization += $word.toLower();
+#                 $correctCaptilization += " ";
+#                 }
+#                 }
+#                 $correctCaptilization
+#     }
+#     $AllCaps{
+#         $i = 0;
+#             foreach($word in $evaluatedWordsArray){
+#                 $word.toUpper();
+#                 $correctCaptilization += $word;
+#                 $correctCaptilization += " ";
+#             }
+#             $correctCaptilization
+#     }
+   
+#     }
 }
